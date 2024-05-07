a7392426670705ad999a2dbc356d00f52736ccd1
Erick Sousa <111514805+Erick-DAS@users.noreply.github.com>
2024-05-07 09:25:16 -0300

    Remove password from mutation output
    Co-authored-by: Alan Raso <alanrraso@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,9 @@ const typeDefs = `#graphql
 
   type User {
     id: ID!
-    name: String
-    email: String
-    password: String
-    birthDate: String 
+    name: String!
+    email: String!
+    birthDate: String
   }
 
   input UserInput {
