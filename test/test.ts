4d884243db9bd5c0008f4902981d41130a7fdaf8
Erick-DAS <erick.sousa@taqtile.com.br>
2024-05-07 17:29:52 -0300

    🚧 ✅  Add test related configurations

---
+++ test/test.ts
@@ -0,0 +1,11 @@
+import assert from 'assert'
+import { describe, it } from 'mocha'
+
+describe('string lenght', function () {
+  it('string lenght of sum should be equal to sum of string lenghts', function () {
+    const first_string = 'aa'
+    const second_string = 'aa'
+    const sum_string = first_string + second_string
+    assert.equal(first_string.length + second_string.length, sum_string.length)
+  })
+})
