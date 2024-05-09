f47d71dc35d4fd32c37ff55140c0e37a9aeed5df
Erick-DAS <erick.sousa@taqtile.com.br>
2024-05-09 08:59:41 -0300

    🎨 Add log for database

--- src/setup-db.ts
+++ src/setup-db.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 export let prisma: PrismaClient
 
 export async function startDb() {
-  console.log('URL DA DATABASE: ' + process.env.DATABASE_URL)
   prisma = new PrismaClient()
   await prisma.$connect()
+  console.log(`🚀  Access database at URL: ${process.env.DATABASE_URL}`)
 }
