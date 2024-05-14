715756b9845624125cd431d6aa927590ba2389b6
Erick-DAS <erick.sousa@taqtile.com.br>
2024-05-14 15:29:36 -0300

    ✨ Implement seed

---
+++ src/seeds/userSeed.ts
@@ -0,0 +1,62 @@
+import { faker } from '@faker-js/faker'
+import setup from '../setup.js'
+import { prisma } from '../setup-db.js'
+import { stopServer } from '../setup-server.js'
+import bcrypt from 'bcrypt' 
+
+await setup()
+
+const users = await generateUniqueUsers(50)
+
+users.forEach((user, index) => {
+  console.log(`User ${index + 1}:`)
+  console.log(`Name: ${user.name}`)
+  console.log(`Email: ${user.email}`)
+  console.log(`Birth Date: ${user.birthDate}`)
+  console.log(`Password: ${user.password}`)
+  console.log('---------------------')
+})
+
+await stopServer()
+
+async function generateUniqueUsers(count: number) {
+  const users = []
+
+  for (let i = 0; i < count; i++) {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const fullName = firstName + lastName
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(faker.internet.password(), salt)
+    const formattedDate = formatDate(
+      faker.date.birthdate({
+        min: 18,
+        max: 65,
+        mode: 'age',
+      })
+    )
+
+    const user = {
+      name: fullName,
+      email: faker.internet.email({ firstName, lastName }),
+      birthDate: formattedDate,
+      password: passwordHash,
+    }
+    users.push(user)
+  }
+
+  await prisma.user.createMany({
+    data: users,
+    skipDuplicates: true,
+  })
+
+  return users
+}
+
+function formatDate(date: Date): string {
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+
+  return `${day}-${month}-${year}`
+}
